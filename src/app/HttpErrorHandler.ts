a42139cf6884bd19ea8eba9cfced1123b5f88244
Jose Mulet Pol <josep@iMac-de-Jose.local>
2018-02-21 18:25:57 +0100

    interceptor 500

--- src/app/HttpErrorHandler.ts
+++ src/app/HttpErrorHandler.ts
@@ -1,16 +1,21 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 @Injectable()
 export class HttpErrorHandler {
 
+      constructor(private growl: MessageService){}
+
       errorCatch(err) {
             if (err instanceof HttpErrorResponse) {
-                  if (err.status === 401 || err.status >= 500) {
+                  if (err.status === 401) {
                         console.log('MUST LOGIN -- session expired');
                         localStorage.removeItem('pwSession');
                         window.location.href = '/pda';
+                  } else if(err.status >= 500) {
+                        this.growl.add({ severity: 'error', summary: err.status +"", detail: err.message });
                   }
             }
             return Observable.throw(err);
